fe7c73962bd0b77749bebd3b41db24447a5d6a35
kacperzaba <zabakacper03@gmail.com>
2024-12-09 23:47:44 +0100

    stripe checkout

--- llm-project-api/llm-project-api/middlewares/validationMiddleware.js
+++
@@ -1,9 +0,0 @@
-import ApiError from "../error/ApiError.js";
-
-export const validateRequest = (schema) => (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-        return next(ApiError.badRequest(error.details[0].message));
-    }
-    next();
-};
